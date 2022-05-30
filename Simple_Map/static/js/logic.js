248281e145d01ea1b937b9d9f89184db74b15ee2
shumph10 <sarahhumphrey2016@outlook.com>
2022-05-30 17:34:58 -0400

    updated marker additions

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -28,3 +28,14 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
 });
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+//  Add a marker to the map for Los Angeles, California.
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+//add a circle marker to the map, then edit to use circleMarker()
+  //to have the radius in pixels not meters
+  L.circleMarker([34.0522, -118.2437], {
+    radius: 100,
+    color:'black',
+    fillColor: '#ffffa1'
+ }).addTo(map);
