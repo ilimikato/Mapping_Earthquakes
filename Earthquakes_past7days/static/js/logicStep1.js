7bfd1eabd3f457a8711fe6d8d9d7a7dcb298d984
shumph10 <sarahhumphrey2016@outlook.com>
2022-06-03 13:20:48 -0400

    map created with markers for every earthquake thats occured in past 7 days

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -17,37 +17,28 @@ attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStree
     maxZoom: 18,
     accessToken: API_KEY
 });
+
 //create a base layer that holds both maps(street & dark)
 let baseMaps = {
   //key is the text that appears on map: value ref the tile layer
   "Streets": streets,
-  "Satellite Streets": satelliteStreets
+  "Satellite": satelliteStreets
 };
 
 //add a map obj with a center and zoom level
 let map = L.map("mapid", {
-  center: [43.7, -79.3],
-  zoom: 10,
+  center: [39.5, -98.5],
+  zoom: 3,
   layers: [streets]
 });
 
 L.control.layers(baseMaps).addTo(map);
 
-let torontoNeighborhood = 'https://raw.githubusercontent.com/shumph10/Mapping_Earthquakes/main/Mapping_GeoJSON_Polygons/torontoNeighborhoods.json'
+let quakeData = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson'
 
 // Grabbing our GeoJSON data.
-d3.json(torontoNeighborhood).then(function(data) {
+d3.json(quakeData).then(function(data) {
   console.log(data);
 // Creating a GeoJSON layer with the retrieved data.
-L.geoJSON(data, {
-  style: function(feature) {
-    return {
-      weight: 1,
-      fillColor: "yellow",
-     }
-  },
-  onEachFeature: function(feature, layer) {
-    layer.bindPopup(`<h3>Neightborhood: ${feature.properties.AREA_NAME}</h3>`)
-  }
-}).addTo(map);
+L.geoJSON(data).addTo(map);
 });
